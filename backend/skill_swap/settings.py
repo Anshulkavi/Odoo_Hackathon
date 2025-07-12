9fb76b4eaa99794fa5aa12bd457bb3b298bcdcb7
Anshulkavi <a234nshul@gmail.com>
2025-07-12 14:02:22 +0530

    Replace all old files with new version

--- backend/skill_swap/settings.py
+++ backend/skill_swap/settings.py
@@ -27,12 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Allow frontend (e.g., Vite, Next.js) to connect:
+CORS_ALLOW_ALL_ORIGINS = True  # For dev only (be cautious in production)
+
 
 # Application definition
 
 INSTALLED_APPS = [
     'rest_framework',
     # 'accounts',
+    "corsheaders",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
